354e762106611c12b014bb1d4fb78f1dbb7d5528
Lucas Costa Santos <lucas26xd@gmail.com>
2020-10-13 15:49:29 -0300

    Melhorias e Limpeza no Código

--- main.py
+++ main.py
@@ -25,33 +25,6 @@ def import_bases():
     return dfOcorrencias, dfPessoas
 
 
-def calc_intervalo_meses_ano(df, months, years):
-    months_ = months
-    list_months = ['Hi!'] * len(years)
-    cont = 0
-    i = 0
-    for y in years:
-        months_ = months_[cont:]
-        cont = 0
-        for m in months_:
-            cont += 1
-            if m == 'December':
-                break
-        list_months[i] = months_[0:cont]
-        i += 1
-    
-    i = 0
-    df_ = df[(df['Ano'].isin([years[i]])) & (df['Mês'].isin(list_months[i]))]
-    if len(years) > 1:
-        for y in years[1:]:
-            i += 1
-            df_1 = df[(df['Ano'].isin([y])) & (df['Mês'].isin(list_months[i]))]
-            frames = [df_, df_1]
-            df_ = pd.concat(frames)
-    
-    return df_
-
-
 st.title('Dados abertos sobre Acidentes da Polícia Rodoviária Federal a partir da aplicação do processo OSEMN')
 st.sidebar.title('Sobre o trabalho:')
 app_mode = st.sidebar.selectbox('Capítulo', ['Introdução', 'Desempenho dos regressores', 'Predições por Faixa Etária', 'Predições por Causas de Acidentes'])
@@ -62,7 +35,7 @@ if app_mode == 'Introdução':
     Introducao()
 elif app_mode == 'Desempenho dos regressores':
     pass
-elif app_mode == 'Predições Idade':
+elif app_mode == 'Predições por Faixa Etária':
     st.sidebar.title('Predições por mes/ano em cada faixa etária')
     st.write('### Legenda das Faixas Etárias:')
     st.write('- Criança: Pessoa com idade entre **0** e **12** anos')
@@ -70,94 +43,45 @@ elif app_mode == 'Predições Idade':
     st.write('- Adulto: Pessoa com idade entre **24** e **60** anos')
     st.write('- Idoso: Pessoa com idade superior a **60** anos')
 
-    range_dates = [f'{mes + 1:0>2}/{2017 + ano}' for ano in range(5) for mes in range(12)]
-    start_date, end_date = st.sidebar.select_slider('Intervalo de datas', options=range_dates, value=('01/2020', '03/2021'))
+    pred = Predicoes(['Acidentes', 'Envolvidos', 'Ilesos', 'Feridos Leves', 'Feridos Graves', 'Mortos'])
 
-    options_qtd = ['Acidentes', 'Envolvidos', 'Ilesos', 'Feridos Leves', 'Feridos Graves', 'Mortos']
-    qtds = st.sidebar.selectbox('Quantidade', options=options_qtd, index=1)
-
-    ppi = PredicoesPessoasIdade(dfPessoas)
-    df_predicts, df = ppi.predicts()
+    ppi = PredicoesPessoasIdade(pred, dfPessoas)
+    df = ppi.predicts()
 
     # Pegando apenas as conlunas requisitadas
-    df = df[(ppi.x + [qtds])]
+    df = df[(ppi.x + [pred.qtds])]
 
     # Pegando apenas as datas requisitadas
-    start_year = int(start_date[3:])
-    start_month = int(start_date[:2])
-    end_year = int(end_date[3:])
-    end_month = int(end_date[:2])
-
-    years = list()
-    for y in range(end_year - start_year + 1):
-        years.append(f'{start_year + y}')
-
-    months = list()
-    qtd_months = (len(years) * 12) - (start_month - 1) - (12 - end_month)
-    for m in range(qtd_months):
-        months.append(f'{ppi.month_names[(start_month + m - 1) % 12]}')
-
-    # df = df[(df['Ano'].isin(years)) & (df['Mês'].isin(months))]
-    df = calc_intervalo_meses_ano(df, months, years)
+    df = pred.filter_dates(df)
 
     #Ordenando
-    sorterIndex = dict(zip(ppi.month_names, range(len(ppi.month_names))))
-    df['mes_rank'] = df['Mês'].map(sorterIndex)
-    df = df.sort_values(['Ano', 'mes_rank'])
-    df = df.drop(columns=['mes_rank'])
+    df = pred.sort(df)
 
     st.write(df)
 
     # Plotagem
-    fig = ppi.create_plot(df, qtds, 'Quantidades por Mês/Ano em cada Faixa Etária')
-    # fig.update_layout(width=1000, height=800)
+    fig = pred.create_plot(df, 'Faixa Etária', 'Quantidades por Mês/Ano em cada Faixa Etária')
     st.write(fig)
 elif app_mode == 'Predições por Causas de Acidentes':
     st.sidebar.title('Predições por Causas de Acidentes')
 
-    range_dates = [f'{mes + 1:0>2}/{2017 + ano}' for ano in range(5) for mes in range(12)]
-    start_date, end_date = st.sidebar.select_slider('Intervalo de datas', options=range_dates, value=('01/2020', '12/2020'))
+    pred = Predicoes(['Acidentes', 'Ilesos', 'Feridos Leves', 'Feridos Graves', 'Mortos', 'Feridos', 'Veículos'])
 
-    options_qtd = ['Acidentes', 'Ilesos', 'Feridos Leves', 'Feridos Graves', 'Mortos', 'Feridos', 'Veículos']
-    #qtds = st.sidebar.multiselect('Quantidade', options=options_qtd, default=['Acidentes'])
-    qtds = st.sidebar.selectbox('Quantidade', options_qtd)
-
-    ppi = PredicoesOcorrenciasCausasAcidentes(dfOcorrencias)
-    df_predicts, df = ppi.predicts()
+    poca = PredicoesOcorrenciasCausasAcidentes(pred, dfOcorrencias)
+    df = poca.predicts()
 
     # Pegando apenas as conlunas requisitadas
-    #df = df[(ppi.x + qtds)]
-    df = df[(ppi.x + [qtds])]
+    df = df[(poca.x + [pred.qtds])]
 
     # Pegando apenas as datas requisitadas
-    start_year = int(start_date[3:])
-    start_month = int(start_date[:2])
-    end_year = int(end_date[3:])
-    end_month = int(end_date[:2])
+    df = pred.filter_dates(df)
 
-    years = list()
-    for y in range(end_year - start_year + 1):
-        years.append(f'{start_year + y}')
-
-    months = list()
-    qtd_months = (len(years) * 12) - (start_month - 1) - (12 - end_month)
-    for m in range(qtd_months):
-        months.append(f'{ppi.month_names[(start_month + m - 1) % 12]}')
-  
-    df = calc_intervalo_meses_ano(df, months, years)
-    
-    #Ordenando
-    sorterIndex = dict(zip(ppi.month_names, range(len(ppi.month_names))))
-    df['mes_rank'] = df['Mês'].map(sorterIndex)
-    df = df.sort_values(['Ano', 'mes_rank'])
-    df = df.drop(columns=['mes_rank'])
+    # Ordenando
+    df = pred.sort(df)
 
     st.write(df)
-    
-    fig = ppi.create_plot(df, qtds, 'Quantidades por Mês/Ano em cada Causa de Acidentes')
-    fig.update_layout(width=1000, height=800)
 
-    #st.plotly_chart(fig,width = 1200, height=800)
+    #Plotagem
+    fig = pred.create_plot(df, 'Causas de Acidentes', 'Quantidades por Mês/Ano em cada Causa de Acidentes')
+    fig.update_layout(width=1000, height=800)
     st.write(fig)
- 
-    #st.write(ppi.create_plot(df, qtds, 'Quantidades por Mês/Ano em cada Causa de Acidentes'))
--- predicoes.py
+++ predicoes.py
@@ -1,24 +1,110 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 import plotly.graph_objects as go
+import streamlit as st
+from sklearn.ensemble import RandomForestRegressor
+
+
+class Predicoes():
+    def __init__(self, options):
+        self.month_names_missing = ['July', 'August', 'September', 'October', 'November', 'December']
+        self.month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                            'October', 'November', 'December']
+        self.y = options
+
+        self.range_dates = [f'{mes + 1:0>2}/{2017 + ano}' for ano in range(5) for mes in range(12)]
+        self.start_date, self.end_date = st.sidebar.select_slider('Intervalo de datas', options=self.range_dates,
+                                                                  value=('01/2020', '03/2021'))
+        self.qtds = st.sidebar.selectbox('Quantidade', options=self.y, index=1)
+
+    def filter_dates(self, df):
+        start_year = int(self.start_date[3:])
+        start_month = int(self.start_date[:2])
+        end_year = int(self.end_date[3:])
+        end_month = int(self.end_date[:2])
+
+        years = list()
+        for y in range(end_year - start_year + 1):
+            years.append(f'{start_year + y}')
+
+        months = list()
+        qtd_months = (len(years) * 12) - (start_month - 1) - (12 - end_month)
+        for m in range(qtd_months):
+            months.append(f'{self.month_names[(start_month + m - 1) % 12]}')
+
+        return self.__calc_intervalo_meses_ano(df, months, years)
+
+    def __calc_intervalo_meses_ano(self, df, months, years):
+        months_ = months
+        list_months = ['Hi!'] * len(years)
+        cont = 0
+        i = 0
+        for y in years:
+            months_ = months_[cont:]
+            cont = 0
+            for m in months_:
+                cont += 1
+                if m == 'December':
+                    break
+            list_months[i] = months_[0:cont]
+            i += 1
+
+        i = 0
+        df_ = df[(df['Ano'].isin([years[i]])) & (df['Mês'].isin(list_months[i]))]
+        if len(years) > 1:
+            for y in years[1:]:
+                i += 1
+                df_1 = df[(df['Ano'].isin([y])) & (df['Mês'].isin(list_months[i]))]
+                frames = [df_, df_1]
+                df_ = pd.concat(frames)
+
+        return df_
+
+    def sort(self, df):
+        sorter_index = dict(zip(self.month_names, range(len(self.month_names))))
+        df['mes_rank'] = df['Mês'].map(sorter_index)
+        df = df.sort_values(['Ano', 'mes_rank'])
+        return df.drop(columns=['mes_rank'])
+
+    def create_plot(self, df, by, title):
+        fig = go.Figure()
+        for faixa in df[by].unique():
+            data = df[df[by] == faixa]
+            meses = [f'{t[0][:3]} {t[1]}' for t in zip(data['Mês'].values, data['Ano'].values)]
+            fig.add_trace(go.Scatter(x=meses, y=data[self.qtds].values, mode='lines+markers', name=faixa))
+        fig.update_layout(title_text=title)
+        return fig
+
+    def predict(self, df, df_new, X_train, X_test):
+        Y_train = df[self.y]
+
+        regressor = RandomForestRegressor()
+        regressor.fit(X_train, Y_train)
+        previsoes = regressor.predict(X_test)
+
+        previsoes = pd.DataFrame(previsoes)
+        previsoes.columns = self.y
+
+        for c in previsoes.columns:
+            previsoes[c] = previsoes[c].astype(int)
+
+        previsoes = pd.concat([df_new, previsoes], axis=1)
+
+        return pd.concat([df, previsoes])
 
 
 class PredicoesPessoasIdade():
-    def __init__(self, dfPessoas):
-        self.month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        self.y = ['Acidentes', 'Envolvidos', 'Ilesos', 'Feridos Leves', 'Feridos Graves', 'Mortos']
+    def __init__(self, pred, dfPessoas):
         self.x = ['Mês', 'Ano', 'Faixa Etária']
+        self.pred = pred
 
         dfPessoas['data_inversa'] = pd.to_datetime(dfPessoas['data_inversa'])
         self.df = dfPessoas.copy()
 
-        self.agg_data(dfPessoas)
-        self.agg_new_data()
+        self.__agg_data(dfPessoas)
+        self.__agg_new_data()
 
-    def agg_data(self, dfPessoas):
+    def __agg_data(self, dfPessoas):
         self.df.loc[dfPessoas['idade'] >= 0, 'idade'] = 'Criança'
         self.df.loc[dfPessoas['idade'] >= 13, 'idade'] = 'Jovem'
         self.df.loc[dfPessoas['idade'] >= 25, 'idade'] = 'Adulto'
@@ -27,17 +113,16 @@ class PredicoesPessoasIdade():
         self.df = self.df.groupby([self.df['data_inversa'].dt.strftime('%B'), self.df['data_inversa'].dt.strftime('%Y'), 'idade'])
         self.df = self.df.agg({'id': 'nunique', 'pesid': 'count', 'ilesos': 'sum', 'feridos_leves': 'sum', 'feridos_graves': 'sum', 'mortos': 'sum'})
         self.df.index.names = self.x
-        self.df.columns = self.y
+        self.df.columns = self.pred.y
         self.df = self.df.reset_index()
 
-    def agg_new_data(self):
-        month_names_missing = ['July', 'August', 'September', 'October', 'November', 'December']
+    def __agg_new_data(self):
         faixa_etaria_unique = self.df['Faixa Etária'].unique().tolist()
 
-        mesh = np.array(np.meshgrid(month_names_missing, ['2020'], faixa_etaria_unique)).T.reshape(-1, 3)
+        mesh = np.array(np.meshgrid(self.pred.month_names_missing, ['2020'], faixa_etaria_unique)).T.reshape(-1, 3)
         df_new2020 = pd.DataFrame(mesh, columns=['Mês', 'Ano', 'Faixa Etária'])
 
-        mesh = np.array(np.meshgrid(self.month_names, ['2021'], faixa_etaria_unique)).T.reshape(-1, 3)
+        mesh = np.array(np.meshgrid(self.pred.month_names, ['2021'], faixa_etaria_unique)).T.reshape(-1, 3)
         df_new2021 = pd.DataFrame(mesh, columns=['Mês', 'Ano', 'Faixa Etária'])
 
         self.df_new = pd.concat([df_new2020, df_new2021])
@@ -46,80 +131,47 @@ class PredicoesPessoasIdade():
         self.df_concatenated = pd.concat([self.df, self.df_new])
 
         # Encode meses
-        for month in self.month_names:
-            self.df_concatenated.loc[self.df_concatenated['Mês'] == month, 'Mês'] = self.month_names.index(month) + 1
+        for month in self.pred.month_names:
+            self.df_concatenated.loc[self.df_concatenated['Mês'] == month, 'Mês'] = self.pred.month_names.index(month) + 1
 
         # Encode faixa etaria
         for faixa in faixa_etaria_unique:
             self.df_concatenated.loc[self.df_concatenated['Faixa Etária'] == faixa, 'Faixa Etária'] = faixa_etaria_unique.index(faixa) + 1
 
         self.df_concatenated.index = [i for i in range(len(self.df_concatenated))]
-        # print(self.df_concatenated)
 
     def predicts(self):
         X_train = self.df_concatenated[:len(self.df)][self.x]
         X_test = self.df_concatenated[len(self.df):][self.x]
-        Y_train = self.df[self.y]
 
-        # poly = PolynomialFeatures(degree=3)
-        # X_poly_train = poly.fit_transform(X_train)
-        # X_poly_test = poly.fit_transform(X_test)
-        #
-        # regressor = LinearRegression()
-        # regressor.fit(X_poly_train, Y_train)
-        #
-        # previsoes = regressor.predict(X_poly_test)
-
-        regressor = RandomForestRegressor()
-        regressor.fit(X_train, Y_train)
-        previsoes = regressor.predict(X_test)
-
-        previsoes = pd.DataFrame(previsoes)
-        previsoes.columns = self.y
-        for c in previsoes.columns:
-            previsoes[c] = previsoes[c].astype(int)
-
-        previsoes = pd.concat([self.df_new, previsoes], axis=1)
-
-        return previsoes, pd.concat([self.df, previsoes])
-
-    def create_plot(self, df, attrs, title):
-        fig = go.Figure()
-        for faixa in df['Faixa Etária'].unique():
-            data = df[df['Faixa Etária'] == faixa]
-            meses = [f'{t[0][:3]} {t[1]}' for t in zip(data['Mês'].values, data['Ano'].values)]
-            fig.add_trace(go.Scatter(x=meses, y=data[attrs].values, mode='lines+markers', name=faixa))
-        fig.update_layout(title_text=title)
-        return fig
+        return self.pred.predict(self.df, self.df_new, X_train, X_test)
 
 
 class PredicoesOcorrenciasCausasAcidentes():
-    def __init__(self, dfOcorrencias):
-        self.month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        self.y = ['Acidentes', 'Ilesos', 'Feridos Leves', 'Feridos Graves', 'Mortos', 'Feridos', 'Veículos']
+    def __init__(self, pred, dfOcorrencias):
         self.x = ['Mês', 'Ano', 'Causas de Acidentes']
+        self.pred = pred
 
         dfOcorrencias['data_inversa'] = pd.to_datetime(dfOcorrencias['data_inversa'])
         self.df = dfOcorrencias.copy()
 
-        self.agg_data(dfOcorrencias)
-        self.agg_new_data()
+        self.__agg_data()
+        self.__agg_new_data()
 
-    def agg_data(self, dfOcorrencias):
+    def __agg_data(self):
         self.df = self.df.groupby([self.df['data_inversa'].dt.strftime('%B'), self.df['data_inversa'].dt.strftime('%Y'), 'causa_acidente'])
         self.df = self.df.agg({'id': 'nunique', 'ilesos': 'sum', 'feridos_leves': 'sum', 'feridos_graves': 'sum', 'mortos': 'sum', 'feridos': 'sum', 'veiculos': 'sum'})
         self.df.index.names = self.x
-        self.df.columns = self.y
+        self.df.columns = self.pred.y
         self.df = self.df.reset_index()
 
-    def agg_new_data(self):
-        month_names_missing = ['July', 'August', 'September', 'October', 'November', 'December']
+    def __agg_new_data(self):
         causa_acidente_unique = self.df['Causas de Acidentes'].unique().tolist()
 
-        mesh = np.array(np.meshgrid(month_names_missing, ['2020'], causa_acidente_unique)).T.reshape(-1, 3)
+        mesh = np.array(np.meshgrid(self.pred.month_names_missing, ['2020'], causa_acidente_unique)).T.reshape(-1, 3)
         df_new2020 = pd.DataFrame(mesh, columns=['Mês', 'Ano', 'Causas de Acidentes'])
 
-        mesh = np.array(np.meshgrid(self.month_names, ['2021'], causa_acidente_unique)).T.reshape(-1, 3)
+        mesh = np.array(np.meshgrid(self.pred.month_names, ['2021'], causa_acidente_unique)).T.reshape(-1, 3)
         df_new2021 = pd.DataFrame(mesh, columns=['Mês', 'Ano', 'Causas de Acidentes'])
 
         self.df_new = pd.concat([df_new2020, df_new2021])
@@ -128,37 +180,17 @@ class PredicoesOcorrenciasCausasAcidentes():
         self.df_concatenated = pd.concat([self.df, self.df_new])
 
         # Encode meses
-        for month in self.month_names:
-            self.df_concatenated.loc[self.df_concatenated['Mês'] == month, 'Mês'] = self.month_names.index(month) + 1
+        for month in self.pred.month_names:
+            self.df_concatenated.loc[self.df_concatenated['Mês'] == month, 'Mês'] = self.pred.month_names.index(month) + 1
 
-        # Encode faixa etaria
+        # Encode causa acidentes
         for causa in causa_acidente_unique:
             self.df_concatenated.loc[self.df_concatenated['Causas de Acidentes'] == causa, 'Causas de Acidentes'] = causa_acidente_unique.index(causa) + 1
 
         self.df_concatenated.index = [i for i in range(len(self.df_concatenated))]
-        # print(self.df_concatenated)
 
     def predicts(self):
         X_train = self.df_concatenated[:len(self.df)][self.x]
         X_test = self.df_concatenated[len(self.df):][self.x]
-        Y_train = self.df[self.y]
 
-        regressor = RandomForestRegressor()
-        regressor.fit(X_train, Y_train)
-        previsoes = regressor.predict(X_test)
-
-        previsoes = pd.DataFrame(previsoes)
-        previsoes.columns = self.y
-
-        previsoes = pd.concat([self.df_new, previsoes], axis=1)
-
-        return previsoes, pd.concat([self.df, previsoes])
-
-    def create_plot(self, df, attrs, title):
-        fig = go.Figure()
-        for causa in df['Causas de Acidentes'].unique():
-            data = df[df['Causas de Acidentes'] == causa]
-            meses = [f'{t[0][:3]} {t[1]}' for t in zip(data['Mês'].values, data['Ano'].values)]
-            fig.add_trace(go.Scatter(x=meses, y=data[attrs].values, mode='lines+markers', name=causa))
-        fig.update_layout(title_text=title)
-        return fig
+        return self.pred.predict(self.df, self.df_new, X_train, X_test)
